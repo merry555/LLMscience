526b08d54df6c7689d5e196c9fbd9ea199e6c508
merry555 <jisukim8873@gmail.com>
2023-09-07 14:13:42 +0900

    orca

--- llm/finetune.py
+++ llm/finetune.py
@@ -8,9 +8,9 @@ from datasets import load_dataset, Dataset
 import torch
 
 import transformers
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, LlamaTokenizerFast, AutoTokenizer
 from transformers import TrainingArguments
-
+import bitsandbytes as bnb
 from peft import LoraConfig
 
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
@@ -36,18 +36,27 @@ def read_config(config_path):
         config = yaml.load(file, Loader=yaml.FullLoader)
     return config
 
+def find_all_linear_names(model):
+  lora_module_names = set()
+  for name, module in model.named_modules():
+    if isinstance(module, bnb.nn.Linear4bit):
+      names = name.split(".")
+      lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+  if "lm_head" in lora_module_names:  # needed for 16-bit
+    lora_module_names.remove("lm_head")
+  return list(lora_module_names)
+
 
 def format_text(example):
-    template = """Answer the following multiple choice question by giving the most appropriate response. Answer should be one among [A, B, C, D, E]
+    template = """### User: Choose the following multiple choice answer by giving the most appropriate response. Answer should be one among [A, B, C, D, E]
 
                 Question: {prompt}\n
                 A) {a}\n
                 B) {b}\n
                 C) {c}\n
                 D) {d}\n
-                E) {e}\n
-
-                Answer: {answer}"""
+                E) {e}\n### Assistant: {answer}"""
 
     prompt = PromptTemplate(template=template, input_variables=['prompt', 'a', 'b', 'c', 'd', 'e', 'answer'])
 
@@ -78,14 +87,10 @@ def train(config):
     tokenizer = AutoTokenizer.from_pretrained(model_id)
     tokenizer.pad_token = tokenizer.eos_token
 
-    qlora_config = LoraConfig(
-        r=16,
-        lora_alpha=32,
-        lora_dropout=0.05,
-        bias="none",
-        target_modules=["up_proj", "down_proj"], # , "dense", "dense_h_to_4h", "dense_4h_to_h"
-        task_type="CAUSAL_LM"
-    )
+    response_template_with_context = "\n### Assistant:"
+    response_template_ids = tokenizer.encode(response_template_with_context, add_special_tokens=False)[2:]
+
+    
 
     bnb_config = BitsAndBytesConfig(
         load_in_4bit=True,
@@ -98,8 +103,21 @@ def train(config):
             model_id,
             quantization_config=bnb_config,
             low_cpu_mem_usage=True,
-            device_map="auto"
+            device_map="auto",
+            use_cache=False
             )
+    
+    
+    modules = find_all_linear_names(base_model.model)
+
+    qlora_config = LoraConfig(
+        r=16,
+        lora_alpha=32,
+        lora_dropout=0.05,
+        bias="none",
+        target_modules=modules, # , "dense", "dense_h_to_4h", "dense_4h_to_h"
+        task_type="CAUSAL_LM"
+    )
     
     training_args = TrainingArguments(
                         output_dir="./Orca-mini-7b", 
@@ -117,7 +135,8 @@ def train(config):
                         optim="paged_adamw_8bit",
                         fp16=True,
                         run_name="baseline-orca-sft",
-                        report_to="wandb"
+                        report_to="wandb",
+                        
                     )
     
 
@@ -130,8 +149,7 @@ def train(config):
                                         peft_config=qlora_config,
                                         dataset_text_field="text",
                                         max_seq_length=2048,
-                                        data_collator=DataCollatorForCompletionOnlyLM(tokenizer=tokenizer, 
-                                                                                    response_template="Answer:"),
+                                        data_collator=DataCollatorForCompletionOnlyLM(response_template_ids, tokenizer=tokenizer),
                                     )
     
     supervised_finetuning_trainer.train()
@@ -142,3 +160,4 @@ if __name__ == "__main__":
     args = parse_args()
     config = read_config(args.config_path)
     train(config)
+    # https://github.com/OFA-Sys/gsm8k-ScRel/blob/7fcb62c4a417370b9cc7860a89c59149205b7598/train_llama2_70b.py#L161
--- llm/shell/train.sh
+++ llm/shell/train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup CUDA_VISIBLE_DEVICES=0 python3 /home/jisukim/LLMscience/llm/finetune.py --config /home/jisukim/LLMscience/llm/configs/config_orca_mini_v3_7b.yaml > output.log &
+CUDA_VISIBLE_DEVICES=0 nohup python3 /home/jisukim/LLMscience/llm/finetune.py --config /home/jisukim/LLMscience/llm/configs/config_orca_mini_v3_7b.yaml > output.log &
