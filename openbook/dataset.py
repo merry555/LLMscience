aed5e3d3025e365ea31197cc5c5ae8617aa0457d
Jisu-Kim <jisukim8873@gmail.com>
2023-09-12 11:03:59 +0900

    Update dataset.py

--- openbook/dataset.py
+++ openbook/dataset.py
@@ -5,12 +5,13 @@ from torch.utils.data import Dataset
 # 사용자 정의 Dataset 클래스 생성
 class ParquetDataset(Dataset):
     def __init__(self, folder_path):
-        self.folder_path = folder_path
-        self.parquet_files = [f for f in os.listdir(folder_path) if f.endswith('.parquet')]
+        file_list = os.listdir(folder_path)
         self.texts = []
+        self.parquet_files = sorted(file_list, key=lambda x: int(x.split('-')[0]))
+        print(self.parquet_files)
 
         for file_name in self.parquet_files:
-            file_path = os.path.join(self.folder_path, file_name)
+            file_path = os.path.join(folder_path, file_name)
             df = pd.read_parquet(file_path)
             self.texts.extend(df['text'].tolist())
 
