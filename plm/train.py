a0f4193deaa5d0085daa9e7439153a6e0ec39c4f
Jisu-Kim <jisukim8873@gmail.com>
2023-09-14 18:14:17 +0900

    Update train.py

--- plm/train.py
+++ plm/train.py
@@ -4,7 +4,7 @@ import numpy as np
 # from colorama import Fore, Back, Style
 from tqdm.notebook import tqdm
 import torch
-from datasets import Dataset
+from torch.utils.data import Dataset, DataLoader
 import gc
 from dataclasses import dataclass
 from transformers import AutoModel, AutoTokenizer, AdamW, get_linear_schedule_with_warmup
@@ -17,7 +17,7 @@ from model import LLMScienceForMultipleChoice
 from dataset import CustomDataset
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 def parse_args():
     parser = argparse.ArgumentParser(description="train")
@@ -40,7 +40,8 @@ def map_at_3(predictions, labels):
     return map_sum / len(predictions)
 
 
-def train(model, train_loader, valid_loader, config):
+def train(model, train_loader, config):
+    device = torch.device(config['model']['device'])
     param_optimizer = list(model.named_parameters())
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
@@ -48,8 +49,8 @@ def train(model, train_loader, valid_loader, config):
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
 
-    num_train_optimization_steps = int(5 * len(train_loader) / TRAIN_CFG['accumulation_steps'])
-    optimizer = AdamW(optimizer_grouped_parameters, lr=3e-5,
+    num_train_optimization_steps = int(5 * len(train_loader) / config['model']['accumulation_steps'])
+    optimizer = AdamW(optimizer_grouped_parameters, lr=2e-6,
                       correct_bias=False)  # To reproduce BertAdam specific behavior set correct_bias=False
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0.05 * num_train_optimization_steps,
                                                 num_training_steps=num_train_optimization_steps)  # PyTorch scheduler
@@ -64,10 +65,14 @@ def train(model, train_loader, valid_loader, config):
 
         for idx, batch in enumerate(train_loader):
             with torch.cuda.amp.autocast():
-                preds, loss = model(batch['input_ids'], batch['attention_masks'], batch['labels'])
+                input_ids = batch['input_ids'].to(device)
+                attention_masks = batch['attention_masks'].to(device)
+                labels = batch['labels'].long.to(device)
+                output = model(input_ids, attention_masks, labels)
+
+            loss = output['loss']
 
             scaler.scale(loss).backward()
-
             scaler.step(optimizer)
             scaler.update()
             optimizer.zero_grad()
@@ -79,26 +84,33 @@ def train(model, train_loader, valid_loader, config):
 
             print("Loss: ", avg_loss)
 
-        model.save_pretrained('./output')
+        torch.save(model.state_dict(), '/home/jisukim/LLMscience/plm/output' + f"{epoch}_epochs.bin")
         print('model save complete!')
 
-        with torch.no_grad():
-            labels = []
-            preds = []
+        # with torch.no_grad():
+        #     labels = []
+        #     preds = []
 
-            for idx, batch in enumerate(valid_loader):
-                with torch.cuda.amp.autocast():
-                    pred, loss = model(batch['input_ids'], batch['attention_masks'], batch['labels'])
-                    label = batch['label']
-                    preds.append(pred.detach().cpu().numpy().tolist())
-                    labels.append(label.tolist())
+        #     for idx, batch in enumerate(valid_loader):
+        #         with torch.cuda.amp.autocast():
+        #             pred, loss = model(batch['input_ids'], batch['attention_masks'], batch['labels'])
+        #             label = batch['label']
+        #             preds.append(pred.detach().cpu().numpy().tolist())
+        #             labels.append(label.tolist())
 
-            print('MAPE@: ', map_at_3(preds, labels))
+        #     print('MAPE@: ', map_at_3(preds, labels))
 
 
 
 def main(config):
-    pass
+    device = torch.device(config['model']['device'])
+    train_dataset = CustomDataset(config, dtype="train")
+    # valid_dataset = CustomDataset(config, dtype="valid")
+    train_dataloader = DataLoader(train_dataset, batch_size=config['data']['batch_size'], shuffle=True, num_workers=1,
+                          pin_memory=False, drop_last=True)
+
+    model = LLMScienceForMultipleChoice(config).to(device)
+    train(model, train_dataloader, config)
 
 if __name__ == "__main__":
     args = parse_args()
