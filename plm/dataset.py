14b2b549fbdd9f0dbd522868342955ed44b3446a
Jisu-Kim <jisukim8873@gmail.com>
2023-09-14 18:15:43 +0900

    Update dataset.py

--- plm/dataset.py
+++ plm/dataset.py
@@ -6,37 +6,41 @@ import torch
 
 # 사용자 정의 Dataset 클래스 생성
 class CustomDataset(Dataset):
-    def __init__(self, config):
-        self.dataset = pd.read_csv(config['data']['file_path']) ## csv 파일 형식
-        self.tokenizer = AutoTokenizer.from_pretrained(config)
+    def __init__(self, config, dtype):
+        if dtype == "train":
+            self.dataset = pd.read_csv(config['data']['train']) ## csv 파일 형식
+        else:
+            self.dataset = pd.read_csv(config['data']['valid']) ## csv 파일 형식
+            
+        self.tokenizer = AutoTokenizer.from_pretrained(config['model']['model_name'])
         self.total_max_len = config['data']['max_length']
 
     def __getitem__(self, index):
         row = self.dataset.iloc[index]
 
         first_sentence = [ "[CLS] " + row['context'] ] * 5    
-        second_sentences = [" #### " + row['prompt'] + " [SEP] " + row[option] + " [SEP]" for option in 'ABCDE']    
+        second_sentences = [" #### " + row['prompt'] + " [SEP] " + str(row[option]) + " [SEP]" for option in 'ABCDE']    
         
         ## 5개에 대한 prompt + context 정보
         encode_output = self.tokenizer(
             first_sentence, second_sentences, truncation='only_first', 
-            max_length=self.total_max_len, add_special_tokens=False
+            max_length=self.total_max_len, add_special_tokens=False,
+            padding=True,
+            return_tensors='pt'
         )
 
         option_to_index = {option: idx for idx, option in enumerate('ABCDE')}
 
         labels = option_to_index[row['answer']]
 
+
         return {
             "input_ids": encode_output['input_ids'].squeeze(),
-            "attention_masks": encode_output['attention_masks'].squeeze(),
-            "labels": torch.FloatTensor([labels]).squeeze()
+            "attention_masks": encode_output['attention_mask'].squeeze(),
+            "labels": torch.LongTensor([labels]).squeeze()
         }
         
         
 
     def __len__(self):
-        return len(self.texts)
-
-    def __getitem__(self, idx):
-        return self.texts[idx]
+        return len(self.dataset)
