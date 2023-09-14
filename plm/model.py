642e5a0bf2aa4962114e7d3922ffea4dc2147f3b
Jisu-Kim <jisukim8873@gmail.com>
2023-09-14 18:15:17 +0900

    Update model.py

--- plm/model.py
+++ plm/model.py
@@ -3,39 +3,41 @@ from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 from transformers import AutoModel
 
-class LLMScienceForMultipleChoice(AutoModel):
+class LLMScienceForMultipleChoice(nn.Module):
     def __init__(self, config):
-        super().__init__(config)
-
-        self.deberta = AutoModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size*5, 1)
-        self.device = config['model']['device']
+        super(LLMScienceForMultipleChoice, self).__init__()
+        self.deberta = AutoModel.from_pretrained(config['model']['model_name'])
+        self.dropout = nn.Dropout(0.1)
+        self.classifier = nn.Sequential(
+            nn.Linear(1024*5, 1024),
+            nn.Dropout(0.1),
+            nn.Linear(1024, 512),
+            nn.Linear(512, 5)
+        )
+        self.device = torch.device(config['model']['device'])
 
     def forward(
         self,
         input_ids,
-        attention_masks,
+        attention_mask,
         labels
     ):
-        first_pooler = self.deberta(input_ids=input_ids[0], attention_masks=attention_masks[0]).pooler_output
-        second_pooler = self.deberta(input_ids=input_ids[1], attention_masks=attention_masks[1]).pooler_output
-        thrid_pooler = self.deberta(input_ids=input_ids[2], attention_masks=attention_masks[2]).pooler_output
-        forth_pooler = self.deberta(input_ids=input_ids[3], attention_masks=attention_masks[3]).pooler_output
-        fifth_pooler = self.deberta(input_ids=input_ids[4], attention_masks=attention_masks[4]).pooler_output
+        # print(input_ids.shape) # torch.Size([16, 5, 512])
+        first_pooler = self.deberta(input_ids=input_ids[:, 0, :], attention_mask=attention_mask[:, 0, :]).last_hidden_state[:, 0, :]
+        second_pooler = self.deberta(input_ids=input_ids[:, 1, :], attention_mask=attention_mask[:, 1, :]).last_hidden_state[:, 0, :]
+        thrid_pooler = self.deberta(input_ids=input_ids[:, 2, :], attention_mask=attention_mask[:, 2, :]).last_hidden_state[:, 0, :]
+        forth_pooler = self.deberta(input_ids=input_ids[:, 3, :], attention_mask=attention_mask[:, 3, :]).last_hidden_state[:, 0, :]
+        fifth_pooler = self.deberta(input_ids=input_ids[:, 4, :], attention_mask=attention_mask[:, 4, :]).last_hidden_state[:, 0, :]
 
         concatenate_pooler = torch.cat((first_pooler, second_pooler, thrid_pooler, forth_pooler, fifth_pooler), dim=1)
 
         logits = self.classifier(concatenate_pooler)
-        reshaped_logits = logits.view(-1, 5)
-
         labels = labels.to(self.device)
+        
         loss_fct = CrossEntropyLoss()
-        loss = loss_fct(reshaped_logits, labels)
+        loss = loss_fct(logits, labels)
 
         return {
-            'preds': reshaped_logits,
+            'preds': logits,
             'loss': loss
         }
-
-
